503292c0833530c737428fa81b61e57af40fe4a2
Babior <jamalbabior@gmail.com>
2025-12-09 18:13:19 +0000

    Implemented ceratin core classes

--- Final OOP/src/main/java/Difficulty.java
+++ Final OOP/src/main/java/Difficulty.java
@@ -1,3 +1,7 @@
+/**
+ * Represents the difficulty level of a quiz question.
+ * Used by Question, QuestionBank, and User for adaptive difficulty feature.
+ */
 public enum Difficulty {
     EASY,
     MEDIUM,
